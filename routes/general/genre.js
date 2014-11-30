997a8179b39a9c8b3667706a402f6bf22e9616a7
Vincent Moore <catalystlive@hotmail.com>
2014-11-30 05:07:49 +0000

    added genre route

--- routes/general/genre.js
+++ routes/general/genre.js
@@ -1,5 +1,11 @@
 module.exports = function(router, passport, manga, user)
 {
+    router.route('/genres')
+    
+        .get(function(req, res) {
+            res.render('partials/genres', {layout: 'layout'});
+        });
+        
     router.route('/genres/:genre_name')
     
         .get(function(req, res){
