ad78751ed86c91332252ed6af6588f321ae911de
Vincent Moore <catalystlive@hotmail.com>
2015-01-09 18:03:48 +0000

    added meta information to search route

--- routes/general/search.js
+++ routes/general/search.js
@@ -10,7 +10,15 @@ module.exports = function(router, passport, manga)
                     res.send(err);
                     return;
                 }
-                res.render('temp/partials/search.html', { layout: 'temp/layout', results: data });
+                res.render('temp/partials/search.html', { 
+                    layout: 'temp/layout', 
+                    results: data,
+                    meta:{ 
+                        title: 'Debonair Manga - Read Manga Online for Free', 
+                        description: 'Read Naruto, One Piece, Attack on Titan and many more manga on the best manga reading platform for free.',
+                        keywords: 'manga, reader, free, naruto, debonair, one, piece, bleach, titan, responsive, online'
+                    } 
+                });
             });
         });
 };
