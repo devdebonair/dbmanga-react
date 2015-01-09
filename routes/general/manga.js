ea924730ee6b024deb7596bd058748a8af50ad77
Vincent Moore <catalystlive@hotmail.com>
2015-01-09 18:03:22 +0000

    added meta information to routes

--- routes/general/manga.js
+++ routes/general/manga.js
@@ -3,7 +3,14 @@ module.exports = function(router, passport, manga, user)
     router.route('/manga')
     
         .get(function(req, res) {
-            res.render('temp/partials/browse-manga', { layout: 'temp/layout' });
+            res.render('temp/partials/browse-manga', { 
+                layout: 'temp/layout',
+                meta:{ 
+                        title: 'Debonair Manga - Read Naruto, One Piece, and Attack on Titan Online for Free', 
+                        description: 'Read Naruto, One Piece, Attack on Titan and many more manga on the best manga reading platform for free.',
+                        keywords: 'manga, reader, free, naruto, debonair, one, piece, bleach, titan, responsive, online'
+                } 
+            });
         });
     
     router.route('/manga/:manga_name')
@@ -16,7 +23,15 @@ module.exports = function(router, passport, manga, user)
                     req.redirect(301,'/');
                     return;
                 }
-                res.render('temp/partials/manga', { layout: 'temp/layout', manga: data[0] });
+                res.render('temp/partials/manga', { 
+                    layout: 'temp/layout', 
+                    manga: data[0],
+                    meta:{ 
+                        title: 'Debonair Manga - Read ' + data[0].title + 'Online for Free', 
+                        description: 'Read Naruto, One Piece, Attack on Titan and many more manga on the best manga reading platform for free.',
+                        keywords: data[0].title + data[0].author + ', manga, reader, free, naruto, debonair, one, piece, bleach, titan, responsive, online'
+                    } 
+                });
             });
         });
         
@@ -30,7 +45,17 @@ module.exports = function(router, passport, manga, user)
                     res.send(err);
                     return;
                 }
-                res.render('temp/partials/reader', { layout: 'temp/layout', title: data[0].title, id: data[0].id, chapterNumber: req.params.chapterNumber });
+                res.render('temp/partials/reader', { 
+                    layout: 'temp/layout', 
+                    title: data[0].title, 
+                    id: data[0].id, 
+                    chapterNumber: req.params.chapterNumber,
+                    meta:{ 
+                        title: 'Debonair Manga - Read ' + data[0].title + 'Online for Free', 
+                        description: 'Read Naruto, One Piece, Attack on Titan and many more manga on the best manga reading platform for free.',
+                        keywords: data[0].title + ', manga, reader, free, naruto, debonair, one, piece, bleach, titan, responsive, online'
+                    } 
+                });
             });
         });
 };
