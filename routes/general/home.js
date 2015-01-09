06dec574238a20d6d155111aa17b687ccc292b96
Vincent Moore <catalystlive@hotmail.com>
2015-01-09 18:02:50 +0000

    added meta info to route

--- routes/general/home.js
+++ routes/general/home.js
@@ -3,6 +3,12 @@ module.exports = function(router, passport, manga, user)
     router.route('/')
     
         .get(function(req, res){
-            res.render('temp/partials/home', { layout: 'temp/layout' });
+            res.render('temp/partials/home', { 
+                layout: 'temp/layout', 
+                meta:{ 
+                    title: 'Debonair Manga - Responsive Online Manga Reader', 
+                    description: 'Read Naruto, One Piece, Attack on Titan and many more manga on the best manga reading platform for free.',
+                    keywords: 'manga, reader, free, naruto, debonair, one, piece, bleach, titan, responsive, online'
+                } });
         });
 };
