bb8673c995767a8f50d164320d0699f757349667
Vincent Moore <catalystlive@hotmail.com>
2015-01-10 19:26:58 +0000

    added bcrypt support for users

--- passport/strategies/local.js
+++ passport/strategies/local.js
@@ -4,38 +4,47 @@ var Admin = require("../../database/models/ModelAdmin");
 
 exports.loginUser = new LocalStrategy(function(req, username, password, callback){
         
-        User.findOne({ username: username }, function(err, user){
+    User.findOne({ username: username }, function(err, user){
+        if(err)
+        {
+            callback(err, null);
+            return;
+        }
+        
+        user.validatePassword(password, function(err, result){
             if(err)
             {
                 callback(err, null);
                 return;
             }
             
-            if(!user || !user.validatePassword(password))
+            if(!result)
             {
                 callback(null, false);
                 return;
             }
             
             callback(null, user);
-        });    
+        });
+        
+    });    
 });
 
 exports.loginAdmin = new LocalStrategy(function(username, password, callback){
         
-        Admin.findOne({ username: username }, function(err, admin){
-            if(err)
-            {
-                callback(err, null);
-                return;
-            }
-            
-            if(!admin || !admin.validatePassword(password))
-            {
-                callback(null, false);
-                return;
-            }
-            
-            callback(null, admin);
-        });    
+    Admin.findOne({ username: username }, function(err, admin){
+        if(err)
+        {
+            callback(err, null);
+            return;
+        }
+        
+        if(!admin || !admin.validatePassword(password))
+        {
+            callback(null, false);
+            return;
+        }
+        
+        callback(null, admin);
+    });    
 });
