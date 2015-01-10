0a8167b8aeb03b9b943882b9be71550ea42cfac6
Vincent Moore <catalystlive@hotmail.com>
2015-01-10 19:23:31 +0000

    placed code in closure for dependency injection

--- database/db.js
+++ database/db.js
@@ -1,29 +1,32 @@
-var mongoose = require("mongoose");
-var db = mongoose.connection;
 var config = require("../config");
 var fs = require("fs");
 
-mongoose.connect( config.database.connection );
-
-db.once('open', function(){
-    console.log('Connected to database.');
-    return;
-});
-
-db.on('error', function( err ){
-    console.log( err );
-    return;
-});
-
-fs.readdir(__dirname + '/models', function(err, files){
-    if(err)
-    {
-        console.log(err);
-        return;
-    }
+module.exports = function(mongoose)
+{
+    var db = mongoose.connection;
     
-    files.forEach( function( element ){
-        require(__dirname + '/models/' + element);
+    mongoose.connect( config.database.connectionString );
+    
+    db.once('open', function(){
+        console.log('Connected to database.');
+        return;
     });
-    return;
-});
+    
+    db.on('error', function( err ){
+        console.log( err );
+        return;
+    });
+    
+    fs.readdir(__dirname + '/models', function(err, files){
+        if(err)
+        {
+            console.log(err);
+            return;
+        }
+        
+        files.forEach( function( element ){
+            require(__dirname + '/models/' + element);
+        });
+        return;
+    });
+};
