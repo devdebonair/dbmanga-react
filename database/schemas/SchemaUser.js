ca79fb6a80d391a2fbcca1d7e648b824c791f2e6
Vincent Moore <catalystlive@hotmail.com>
2015-01-10 19:25:16 +0000

    updated user schema with strict rules, premium, preferences, public, and library, and provided bcrypt support

--- database/schemas/SchemaUser.js
+++ database/schemas/SchemaUser.js
@@ -1,15 +1,56 @@
 var Schema = require("mongoose").Schema;
+var bcrypt = require("bcrypt");
 
 var User = new Schema({
-    username: String,
+    username: { type: String, lowercase: true },
     password: String,
-    email: String,
-    avatar: String
+    email: { type: String, lowercase: true },
+    avatar: String,
+    dob: { type: Date, required: true },
+    isPremium: { type: Boolean, default: false },
+    isPublic: Boolean,
+    library: [{
+        id: String,
+        bookmark: {
+            chapter: Number,
+            page: Number
+        }
+    }],
+    preferences: {
+        isDual: { type: Boolean, default: false }
+    }
 });
 
-User.methods.validatePassword = function(password)
+User.pre('save', function(next){
+    var user = this;
+    
+    if(!user.isModified('password'))
+    {
+        next();
+        return;
+    }
+    
+    bcrypt.hash(user.password, 10, function(err, hash){
+        if(err)
+        {
+            next(err);
+            return;
+        }
+        user.password = hash;
+        next();
+    });
+});
+
+User.methods.validatePassword = function(password, callback)
 {
-    return (this.password === password);
+    bcrypt.compare(password, this.password, function(err, result){
+        if(err)
+        {
+            callback(err, false);
+            return;
+        }
+        callback(null, result);
+    });
 };
 
 module.exports = User;
