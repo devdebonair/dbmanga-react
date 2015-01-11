6bc21c4fcb7d0e04c81532d4e3a33b661a41f28a
Vincent Moore <catalystlive@hotmail.com>
2015-01-11 23:58:56 +0000

    added library route

--- routes/api/user.js
+++ routes/api/user.js
@@ -70,4 +70,46 @@ module.exports = function(router)
 			    res.json({ message: 'Successful.' });
 		    });
         });
+        
+    router.route('/users/:user_id/library')
+    
+        .get(function(req, res){
+            
+            User.findById(req.params.user_id, function(err, user) {
+                if(err)
+                {
+                    res.send(err);
+                    return;
+                }
+                res.json(user.library);
+            });
+        })
+        
+        .post(function(req, res){
+            console.log(req.body);
+            User.findByIdAndUpdate(req.params.user_id, { $push: { library: req.body } },
+                function(err, user){
+                    if(err)
+                    {
+                        res.send(err);
+                        return;
+                    }
+                    res.json({ message: 'Success.', user: user });
+                });
+        })
+        
+    router.route('/users/:user_id/library/:book_id')
+    
+        .delete(function(req, res){
+            
+            User.findByIdAndUpdate(req.params.user_id, { $pull : { library: { book_id: req.params.book_id } } }, 
+                function(err, user){
+                    if(err)
+                    {
+                       res.send(err);
+                       return;
+                    }
+                    res.json({ message: 'Successful', user: user });
+                });
+        });
 };
