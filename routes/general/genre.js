31c5bf18a87bd14e09953f3aa8c4498a08f8d7e8
Vincent Moore <catalystlive@hotmail.com>
2015-01-11 19:57:13 +0000

    added user info to genre route

--- routes/general/genre.js
+++ routes/general/genre.js
@@ -3,12 +3,18 @@ module.exports = function(router, passport, manga, user)
     router.route('/genres')
     
         .get(function(req, res) {
-            res.render('partials/genres', {layout: 'layout'});
+            res.render('partials/genres', {
+                layout: 'layout',
+                user: req.user
+            });
         });
         
     router.route('/genres/:genre_name')
     
         .get(function(req, res){
-            res.render('partials/home', {layout: 'layout'});
+            res.render('partials/home', {
+                layout: 'layout',
+                user: req.user
+            });
         });
 };
