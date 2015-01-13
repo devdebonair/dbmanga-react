01ccc403a549ec05bf426e61607a0a6019c05d0a
Vincent Moore <catalystlive@hotmail.com>
2015-01-13 19:48:28 +0000

    added manga query for genre route

--- routes/general/genre.js
+++ routes/general/genre.js
@@ -12,9 +12,22 @@ module.exports = function(router, passport, manga, user)
     router.route('/genres/:genre_name')
     
         .get(function(req, res){
-            res.render('partials/home', {
-                layout: 'layout',
-                user: req.user
+            manga.find({ genres: { $elemMatch: [req.params.genre_name] } }, '-sources', function(err, data){
+                if(err)
+                {
+                    res.send(err);
+                    return;
+                }
+                res.render('temp/partials/genre', {
+                    layout: 'temp/layout',
+                    user: req.user,
+                    results: data,
+                    meta:{ 
+                        title: 'Debonair Manga - Read ' + data.title + ' Online for Free', 
+                        description: 'Read Naruto, One Piece, Attack on Titan and many more manga on the best manga reading platform for free.',
+                        keywords: data.title + ', manga, reader, free, naruto, debonair, one, piece, bleach, titan, responsive, online'
+                    }
+                });
             });
         });
 };
