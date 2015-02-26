f3d4dd25c63043d0d353c79df674a0c156afe023
Vincent Moore <catalystlive@hotmail.com>
2015-02-26 01:06:29 +0000

    removed unnecessary external services

--- server.js
+++ server.js
@@ -13,60 +13,73 @@ var expressLayouts = require("express-ejs-layouts");
 var config = require("./config");
 var passport = require("passport");
 
-// Initialize database
-console.log('Connecting to Database....');
-require("./database/db")(mongoose, function(err){
-   
-   //Configure Express
-    console.log('Configuring Express....');
-    app.use(function(req, res, next) { 
-        if(req.headers.host === 'dbmanga.com') 
-        { 
-            res.writeHead(303, {'Location': 'http://www.dbmanga.com' + req.url});
-            res.end();
-        }
-        else
-        {
-            next();
-        }
-    }); 
-    app.use( express.static(__dirname + '/public') );
-    app.use( '/lib', express.static(__dirname + '/public/static') );
-    app.set( 'views', __dirname + '/public/views/' );
-    app.engine( 'html', require("ejs").__express );
-    app.set( 'view engine', 'html' );
-    
-    app.use( expressLayouts );
-    app.set("layout extractScripts", true);
-    app.use( cookieParser() );
-    app.use( bodyParser.json() );
-    app.use( bodyParser.urlencoded( { extended: false } ) );
-    
-    if(!err)
-    {
-        app.use( session({
-            secret: config.session.secret,
-            store: new MongoStore({
-                mongooseConnection: mongoose.connection
-            }),
-            saveUninitialized: true,
-            resave: false
-        }));
+
+//Configure Express
+console.log('Configuring Express....');
+app.use(function(req, res, next) { 
+    if(req.headers.host === 'dbmanga.com') 
+    { 
+        res.writeHead(303, {'Location': 'http://www.dbmanga.com' + req.url});
+        res.end();
     }
-    app.use( passport.initialize() );
-    app.use( passport.session() );
-    app.use( methodOverride() );
+    else
+    {
+        next();
+    }
+}); 
+app.use( express.static(__dirname + '/public') );
+app.use( '/lib', express.static(__dirname + '/public/static') );
+app.set( 'views', __dirname + '/public/views/' );
+app.engine( 'html', require("ejs").__express );
+app.set( 'view engine', 'html' );
+
+app.use( expressLayouts );
+app.set("layout extractScripts", true);
+app.use( cookieParser() );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( methodOverride() );
+
+// Initialize database
+// console.log('Connecting to Database....');
+// require("./database/db")(mongoose, function(err){
+   
+   
+    
+    // if(!err)
+    // {
+    //     app.use( session({
+    //         secret: config.session.secret,
+    //         store: new MongoStore({
+    //             mongooseConnection: mongoose.connection
+    //         }),
+    //         saveUninitialized: true,
+    //         resave: false
+    //     }));
+    // }
+    // app.use( passport.initialize() );
+    // app.use( passport.session() );
+    
     
     // Initialize Passport
-    console.log('Checking Passport...');
-    require("./passport")(passport);
+    // console.log('Checking Passport...');
+    // require("./passport")(passport);
     
     // Initialize Routes
-    console.log('Establishing Routes....');
-    require("./routes")(app, passport);
+    // console.log('Establishing Routes....');
+    // require("./routes")(app, passport);
     
-    console.log('Starting Server....');
-    http.listen( config.env.port, function(){
-        console.log('Listening to port:\t%s', config.env.port );
-    }); 
+app.get('/', function(req, res){
+    res.render('landing', {
+        layout: false
+    });
+});
+
+app.get('*', function(req, res){
+    res.redirect(301, '/');
+});
+
+console.log('Starting Server....');
+http.listen( config.env.port, function(){
+    console.log('Listening to port:\t%s', config.env.port );
 });
