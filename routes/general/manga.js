82bac7392415b9e7cbc404635fc676055b227b5c
Vincent Moore <catalystlive@hotmail.com>
2015-03-21 19:53:17 +0000

    moved browse route to own file

--- routes/general/manga.js
+++ routes/general/manga.js
@@ -52,18 +52,6 @@ module.exports = function(router, passport, manga, user)
             });
         });
     
-    router.route('/browse')
-        .get(function(req, res) {
-            
-            res.render('partials/browse', { 
-                layout: 'layout',
-                meta:{ 
-                    title: 'Debonair Manga - Read Online for Free', 
-                    description: 'Read Naruto, One Piece, Attack on Titan and many more manga on the best manga reading platform for free.',
-                    keywords: ''
-                }
-            });
-        });
     // router.route('/manga/:manga_name/:chapterNumber')
         
     //     .get(function(req, res){
