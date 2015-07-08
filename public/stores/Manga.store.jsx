b6280a2e3a17b3f931b9bb4da4cceec948f44ad4
Vincent Moore <catalystlive@hotmail.com>
2015-07-08 18:20:05 +0000

    added logic for Directory.actions

--- public/stores/Manga.store.jsx
+++ public/stores/Manga.store.jsx
@@ -5,33 +5,96 @@ var DirectoryActions    = require("../actions/Directory.action.jsx");
 
 var MangaStore = Reflux.creatStore({
     listenables: [DirectoryActions],
-    onGetBook: function()
+    onGetBook: function(book_id)
     {
-        
+        var url = '/api/v1/manga/'+ book_id; 
+        $.get(url, function(err, data) {
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            console.log(data);
+        });
     },
     onGetFeaturedBooks: function()
     {
-        
+        $.get('/api/v1/manga/featured', function(err, data) {
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            console.log(data);
+        });
     },
     onGetUpdatedBooks: function()
     {
-        
+        $.get('/api/v1/manga/updated', function(err, data) {
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            console.log(data);
+        });
     },
     onGetPopularBooks: function()
     {
-        
+        $.get('/api/v1/manga/popular', function(err, data) {
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            console.log(data);
+        });
     },
     getTrendingBooks: function()
     {
-        
+        $.get('/api/v1/manga/trending', function(err, data) {
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            console.log(data);
+        });
     },
     getDirectoryTitles: function()
     {
-        
+        $.get('/api/v1/manga/directory', function(err, data){
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            console.log(data);
+        });
     },
-    onSearchBooks: function()
+    onSearchBooks: function(data)
     {
+        var title = data.title || '';
+        var status = data.status || 'ongoing';
+        var genres = data.genres || [];
+        var min = data.min || null;
+        var max = data.max || null;
         
+        var url = '/api/v1/manga/search?';
+        url += 'title=' + title;
+        url += 'genres=' + genres.join('+');
+        url += 'status=' + status;
+        url += 'min=' + min;
+        url += 'max=' + max;
+        
+        $.get(url, function(err, data){
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            console.log(data);
+        });
     }
 });
 
