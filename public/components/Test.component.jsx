767c22584752e1a02a525ec710190238d0d0b270
Vincent Moore <debonair@Malenas-iMac.att.net>
2015-07-10 08:50:56 -0400

    added more api calls to test component

--- public/components/Test.component.jsx
+++ public/components/Test.component.jsx
@@ -5,14 +5,44 @@ var MangaStore = require('../stores/Manga.store.jsx');
 
 var Test = React.createClass({
 	mixins: [Reflux.connect(MangaStore)],
-	clickHandler: function()
+	directory: function()
 	{
 		DirectoryActions.getDirectoryTitles();
 	},
+    search: function()
+    {
+        var data = {
+            title: 'Na'
+        }
+        DirectoryActions.searchBooks(data);
+    },
+    popular: function()
+    {
+        DirectoryActions.getPopularBooks();
+    },
+    trending: function()
+    {
+        DirectoryActions.getTrendingBooks();
+    },
+    updated: function()
+    {
+        DirectoryActions.getUpdatedBooks();
+    },
+    book: function()
+    {
+        DirectoryActions.getBook('55141ca866fa200f2c4576ee');
+    },
     render: function()
     {
         return(
-            <div><button onClick={this.clickHandler}>Directory Titles</button></div>    
+            <div>
+                <div><button onClick={this.directory}>Directory Titles</button></div>
+                <div><button onClick={this.search}>Search Books</button></div>
+                <div><button onClick={this.popular}>Popular Books</button></div>
+                <div><button onClick={this.trending}>Trending Books</button></div>
+                <div><button onClick={this.updated}>Updated Books</button></div>
+                <div><button onClick={this.book}>Book</button></div>
+            </div>
         );
     }
 });
