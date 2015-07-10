7090633abe5da66da874082c5cb712a1f26469c1
Vincent Moore <vincentmoore@Vincents-MBP.att.net>
2015-07-09 23:39:02 -0400

    added async directory actions to test component

--- public/components/Test.component.jsx
+++ public/components/Test.component.jsx
@@ -3,12 +3,16 @@ var Reflux = require("reflux");
 var DirectoryActions = require('../actions/Directory.action.jsx');
 var MangaStore = require('../stores/Manga.store.jsx');
 
-
 React.createClass({
+	mixins: [Reflux.connect(MangaStore)],
+	clickHandler: function()
+	{
+		DirectoryActions.getDirectoryTitles();
+	},
     render: function()
     {
         return(
-            <div><button>Directory Titles</button></div>    
+            <div><button onClick={clickHandler}>Directory Titles</button></div>    
         );
     }
 });
