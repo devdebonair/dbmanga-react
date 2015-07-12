ea059c0605cdaa567fed964f5e5341bc63c65297
Vincent Moore <debonair@Malenas-iMac.att.net>
2015-07-12 17:43:16 -0400

    add search for select

--- public/components/select/Select.component.jsx
+++ public/components/select/Select.component.jsx
@@ -3,19 +3,22 @@ var Stylesheet = require('./select.css');
 
 var Select = React.createClass({
 	propTypes: {
+		title: React.PropTypes.string,
 		items: React.PropTypes.array,
 		selected: React.PropTypes.object
 	},
 	getDefaultProps: function()
 	{
 		return {
-			items: [{label: 'option 1', value: 1},{label: 'option 2	', value: 2	},{label: 'option 3', value: 3}]
+			items: [],
+			title: 'Dropdown'
 		};
 	},
 	getInitialState: function()
 	{
 		return {
-			isOpen: false
+			isOpen: false,
+			selected: {label: '', value: ''}
 		};
 	},
 	open: function()
@@ -32,20 +35,32 @@ var Select = React.createClass({
 	},
 	select: function(item)
 	{
-
+		for(var i = 0; i < this.props.items.length; i++)
+		{
+			if(this.props.items[i].value === item.value && this.props.items[i].label === item.label)
+			{
+				this.setState({selected: this.props.items[i]});
+				break;
+			}
+		}
+	},
+	clickSelectHandler: function(item, event)
+	{
+		this.select(item);
 	},
 	render: function()
 	{
 		return(
 			<div>
-				<div className="dropdown-trigger" onClick={this.toggle}>Open</div>
+				<div className="dropdown-trigger" onClick={this.toggle}>{this.props.title}</div>
 				<div className={this.state.isOpen ? 'dropdown-menu' : 'no-display'}>
 					<ul className="dropdown-list">
 						{this.props.items.map(function(item, index){
-							return <li key={index} value={item.value} onClick={clickSelectHandler}>{item.label}</li>;
-						})}
+							return <li key={index} onClick={this.clickSelectHandler.bind(null, {label: item.label, value: item.value})}>{item.label}</li>;
+						}.bind(this))}
 					</ul>
 				</div>
+				<div>Selected: {this.state.selected.value}</div>
 			</div>
 		);
 	}
