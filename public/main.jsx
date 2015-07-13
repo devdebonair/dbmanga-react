809b2915fc9cfb6259d810f24ecafc1aee41dd41
Vincent Moore <vincentmoore@Vincents-MBP.att.net>
2015-07-13 07:15:09 -0400

    i really should do more commits in the beginning

--- public/main.jsx
+++ public/main.jsx
@@ -1,4 +1,4 @@
 var React = require('react');
-var Select = require('./components/select/Select.component.jsx');
+var Select = require('./components/theme-select/Select.component.jsx');
 
 React.render(<Select title="GENRES" items={[{label: 'Shounen', value: 'SHOUNEN'},{label: 'Harem', value: 'HAREM'},{label: 'Seinen', value: 'SEINEN'}]}/>, document.getElementById('app'));
