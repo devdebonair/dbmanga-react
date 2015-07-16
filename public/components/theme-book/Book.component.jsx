938050e4c7cf24936ad5962da171aaf55747de4a
Vincent Moore <debonair@Malenas-iMac.att.net>
2015-07-16 17:19:59 -0400

    added theme-book component

---
+++ public/components/theme-book/Book.component.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var Stylesheet = require('./book.css');
+
+module.exports = React.createClass({
+	propTypes: {
+		url: React.PropTypes.string,
+		width: React.PropTypes.number
+	},
+	getDefaultProps: function()
+	{
+		return {
+			url: '',
+			width: 500
+		};
+	},
+	render: function()
+	{
+		return(
+			<div style={{width: this.props.width}}>
+				<img src={this.props.url} />
+			</div>
+		);
+	}
+});
