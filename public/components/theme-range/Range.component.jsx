ea0f0a90dd5597c778bbdf48d040bd7aad8bd00f
Vincent Moore <debonair@Malenas-iMac.att.net>
2015-07-16 21:01:22 -0400

    added theme-range

---
+++ public/components/theme-range/Range.component.jsx
@@ -0,0 +1,41 @@
+var React = require('react');
+var Range = require('../core-range/Range.component.jsx');
+var Stylesheet = require('./range.css');
+
+module.exports = React.createClass({
+	propTypes: {
+		value: React.PropTypes.number,
+		max: React.PropTypes.number,
+		min: React.PropTypes.number,
+		step: React.PropTypes.number,
+		onDebounce: React.PropTypes.func,
+		onChange: React.PropTypes.func
+	},
+	getDefaultProps: function()
+	{
+		return {
+			value: 50,
+			max: 100,
+			min: 0,
+			step: 1
+		};
+	},
+	getInitialState: function()
+	{
+		return {
+			value: 0
+		}
+	},
+	componentWillMount: function()
+	{
+		this.setState({value: this.props.value});
+	},
+	render: function()
+	{
+		return(
+			<div>
+				<Range className="range" onChange={this.props.onChange} onDebounce={this.props.onDebounce} value={this.state.value} min={this.props.min} max={this.props.max} step={this.props.step} />
+			</div>
+		);
+	}
+});
