ed16d139f815d8f71e2a558909c08c9d69635fd2
Vincent Moore <catalystlive@hotmail.com>
2015-07-21 09:36:30 -0400

    added book item component to encapsulate data

---
+++ public/components/debonair-book-item/BookItem.component.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var Stylesheet = require('./book-item.css');
+
+module.exports = React.createClass({
+	propTypes: {
+		title: React.PropTypes.string,
+		coverUrl: React.PropTypes.string,
+		length: React.PropTypes.number,
+		author: React.PropTypes.string,
+		status: React.PropTypes.string,
+		genres: React.PropTypes.arrayOf(React.PropTypes.string),
+		id: React.PropTypes.string,
+		views: React.PropTypes.number,
+		chapters: React.PropTypes.arrayOf(React.PropTypes.object),
+		onClick: React.PropTypes.func
+	},
+	getDefaultProps: function()
+	{
+		return {
+			title: '',
+			coverUrl: '',
+			length: 0,
+			author: '',
+			status: '',
+			genres: [],
+			id: '',
+			views: 0,
+			chapters: [],
+			onClick: function(){}
+		};
+	},
+	onClickHandler: function()
+	{
+		this.props.onClick(this.props);
+	},
+	render: function()
+	{
+		return(
+			<div onClick={this.onClickHandler} className="book-cover-wrapper"><img src={this.props.coverUrl} className="book-cover" /></div>
+		);
+	}
+});
