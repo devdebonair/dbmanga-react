c645b03f22c3ef42a05ee90b359d1b89acf26a73
Vincent Moore <catalystlive@hotmail.com>
2015-07-30 07:38:30 -0400

    added client actions; added logic for getChapter, getBook, defaultData, setSelectedBook, clearSelectedBook; added new defaults

--- public/stores/Manga.store.jsx
+++ public/stores/Manga.store.jsx
@@ -1,15 +1,72 @@
 var Reflux              = require('reflux');
 var DirectoryActions    = require("../actions/Directory.action.jsx");
+var ClientActions       = require('../actions/Client.action.jsx');
 
 var MangaStore = Reflux.createStore({
-    listenables: [DirectoryActions],
+    listenables: [DirectoryActions, ClientActions],
+    init: function()
+    {
+        this.data = {
+            books: [],
+            selectedBook: {
+                id: '',
+                coverUrl: '',
+                genres: [],
+                author: '',
+                summary: '',
+                views: 0,
+                length: 0,
+                status: '',
+                title: '',
+                chapters: []
+            },
+            selectedChapter: {
+                number: 0,
+                pages: [],
+                title: ''
+            }
+        };
+    },
+    onSetSelectedBook: function(data)
+    {
+        this.data.selectedBook = data;
+        this.trigger(this.data);
+    },
+    onClearSelectedBook: function()
+    {
+        this.data.selectedBook = {
+            coverUrl: '',
+            genres: [],
+            author: '',
+            summary: '',
+            views: 0,
+            length: 0,
+            status: '',
+            title: '',
+            chapters: []
+        };
+        
+        this.data.selectedChapter = {
+            number: 0,
+            pages: [],
+            title: ''
+        };
+
+        this.trigger(this.data);
+    },
+    onGetChapterCompleted: function(res)
+    {
+        this.data.selectedChapter = res.body;
+        this.trigger(this.data);
+    },
     onGetDirectoryTitlesCompleted: function(res)
     {
         console.log(res.body);
     },
     onSearchBooksCompleted: function(res)
     {
-    	console.log(res.body);
+        this.data.books = res.body;
+    	this.trigger(this.data);
     },
     onGetPopularBooksCompleted: function(res)
     {
@@ -25,7 +82,12 @@ var MangaStore = Reflux.createStore({
     },
     onGetBookCompleted: function(res)
     {
-    	console.log(res.body);
+        this.data.selectedBook = res.body;
+    	this.trigger(this.data);
+    },
+    getDefaultData: function()
+    {
+        return this.data;
     }
 });
 
