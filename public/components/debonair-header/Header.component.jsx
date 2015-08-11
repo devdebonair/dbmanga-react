2f4b8d0937fb92c66d35268db7c34e8d43298868
Vincent Moore <catalystlive@hotmail.com>
2015-08-11 07:56:18 -0400

    removed dropdown

--- public/components/debonair-header/Header.component.jsx
+++ public/components/debonair-header/Header.component.jsx
@@ -29,28 +29,10 @@ module.exports = Header = React.createClass({
 		this.props.onTitleClick();
 		this.refs.search.focus();
 	},
-	items: [
-		{label: 'Shounen', value: 'shounen'},
-		{label: 'Sienen', value: 'sienen'},
-		{label: 'Adventure', value: 'adventure'},
-		{label: 'Slice of life', value: 'slice of life'},
-		{label: 'Romance', value: 'romance'},
-		{label: 'Harem', value: 'harem'},
-		{label: 'Shoujo', value: 'shoujo'},
-		{label: 'Ecchi', value: 'ecchi'},
-		{label: 'Mature', value: 'mature'}
-	],
-	swagger: function(swagger)
-	{
-		console.log(swagger);
-	},
 	render: function()
 	{
 		return(
 			<section id="home-header">
-				<div className="home-browse">
-					<Dropdown onSelect={this.swagger} title="browse" items={this.items} />
-				</div>
 				<div id="home-actions">
 					<div id="home-title">
 						<span onClick={this.titleClickHandler}>{this.props.title}</span>
